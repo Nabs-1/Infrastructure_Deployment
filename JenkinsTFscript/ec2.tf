93e17eaee940bbf897b8efe1a8a7bbc8fa3cd816
Nabs-1 <nabilah.aralepo@gmail.com>
2025-03-22 08:45:26 -0500

    Update ec2.tf

--- JenkinsTFscript/ec2.tf
+++ JenkinsTFscript/ec2.tf
@@ -85,7 +85,7 @@ data "aws_ami" "ubuntu" {
 
 # launch the ec2 instance
 resource "aws_instance" "ec2_instance" {
-  ami                    = data.aws_ami.ubuntu.id
+  ami                    = ami-08529db39844c00c2
   instance_type          = "t2.small"
   subnet_id              = aws_default_subnet.default_az1.id
   vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
